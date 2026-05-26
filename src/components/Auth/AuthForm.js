2cc9a397e385aaeaffe770afd0070983885a9d16
gaganbedi771 <gaganbedi771@gmail.com>
2026-05-26 22:16:04 +0530

    signup added

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,127 @@
+import { useState, useRef } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [sendingRequest, setSendingRequest] = useState(false);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const loginHandler = async (event) => {
+    event.preventDefault();
+    setSendingRequest((pre) => !pre);
+
+    if (isLogin) {
+      try {
+        const response = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjN7mZ1axTHHgEQNmj-2DQRlzEmEGyGH",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email,
+              password,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        );
+
+        if (!response.ok) {
+          throw new Error("Authentication failed!, " + response.data.error.message);
+        }
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        setSendingRequest((pre) => !pre);
+      }
+    } else {
+      try {
+        const response = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjN7mZ1axTHHgEQNmj-2DQRlzEmEGyGH",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email,
+              password,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        );
+
+       if (!response.ok) {
+          throw new Error("Authentication failed!, " + response.data.error.message);
+        }
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log("else");
+        alert(error.message);
+      } finally {
+        setSendingRequest((pre) => !pre);
+      }
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={loginHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+          />
+        </div>
+
+        <div className={classes.actions}>
+          {sendingRequest ? (
+            <p>Sending request...</p>
+          ) : (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
